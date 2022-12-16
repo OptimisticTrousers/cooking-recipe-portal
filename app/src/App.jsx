2af86213d2fe086561979b9b6e0b6fb609670087
OptimisticTrousers <tony.isern1@gmail.com>
2022-12-16 15:26:12 -0500

    Pushing boilerplate code up to Github

---
+++ app/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import Hero from "./pages/Home";
+import Navbar from "./components/Navbar/Navbar";
+import { Outlet } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ app/src/main.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
+import Home from "./pages/Home/index";
+import Posts from "./pages/Posts/Posts";
+import CreatePost from "./pages/CreatePost/CreatePost";
+import Post from "./components/Post/Post";
+import SinglePostPage from "./pages/SinglePostPage/SinglePostPage";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="posts" >
+            <Route index element={<Posts />}  />
+            <Route path="create" element={<CreatePost />} />
+            <Route path=":id" element={<SinglePostPage/>} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
