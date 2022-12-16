c7334301d7298ffb45e52d5cdff0d9d059708bff
OptimisticTrousers <tony.isern1@gmail.com>
2022-12-16 17:49:23 -0500

    Changing Navbar

--- app/src/components/Navbar/Navbar.tsx
+++ app/src/components/Navbar/Navbar.tsx
@@ -32,29 +32,17 @@ const Navbar = () => {
           <Link to="/" className="navbar-item">
             Home
           </Link>
-          <div className="navbar-item has-dropdown is-hoverable">
-          <a className="navbar-link">Categories</a>
-
-          <div className="navbar-dropdown">
-            <a className="navbar-item">Desserts</a>
-            <a className="navbar-item">Bakery</a>
-            <a className="navbar-item">Pasta</a>
-          </div>
-        </div>
         </div>
         
 
         <div className="navbar-end">
           <div className="navbar-item">
             <div className="buttons">
-              <Link to="/category/create" className="button is-primary">
-                <strong>Create Category</strong>
-              </Link>
-              <Link to="/posts/create" className="button is-primary">
-                <strong>Create Post</strong>
-              </Link>
               <Link to="/posts" className="button is-light">
                 Posts
+              </Link>
+              <Link to="/categories" className="button is-light">
+                Categories 
               </Link>
             </div>
           </div>
