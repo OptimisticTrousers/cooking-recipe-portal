9c04b34efd36404552c16978ec966ebe620d43d2
OptimisticTrousers <tony.isern1@gmail.com>
2022-12-16 19:08:04 -0500

    Creating modals

---
+++ app/src/components/PostModal/PostModal.jsx
@@ -0,0 +1,75 @@
+import {
+  Button,
+  ChakraProvider,
+  FormControl,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Box,
+} from "@chakra-ui/react";
+import { createEditor } from "slate";
+import { Slate, Editable, withReact } from "slate-react";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+const PostModal = ({ isOpen, onClose, overlay }) => {
+  const initialRef = React.useRef(null);
+  const finalRef = React.useRef(null);
+
+  const [editor] = useState(() => withReact(createEditor()));
+
+  return (
+    <ChakraProvider>
+      <Modal
+        initialFocusRef={initialRef}
+        finalFocusRef={finalRef}
+        isOpen={isOpen}
+        onClose={onClose}
+      >
+        {overlay}
+        <ModalOverlay />
+        <ModalContent maxW="800">
+          <ModalHeader>Create a post!</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormControl>
+              <FormLabel>Title</FormLabel>
+              <Input ref={initialRef} placeholder="First Class Pancake Recipe" />
+            </FormControl>
+
+            <FormControl mt={4}>
+              <FormLabel>Author</FormLabel>
+              <Input placeholder="Bob Jones" />
+            </FormControl>
+          </ModalBody>
+
+          <Box px={6}>
+            <FormControl>
+              <FormLabel>Content</FormLabel>
+              <ReactQuill theme="snow" value="<p>Hello from CS 306!</p>" />
+            </FormControl>
+          </Box>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3}>
+              Save
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </ChakraProvider>
+  );
+};
+
+export default PostModal;
