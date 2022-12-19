04f5fea2cf1646f1c9af5d416e4b1aa8e516a3f1
OptimisticTrousers <tony.isern1@gmail.com>
2022-12-19 13:17:28 -0500

    Cleaning up server code

---
+++ server/routes/recipes.js
@@ -0,0 +1,61 @@
+const express = require("express");
+
+const mysqlConnect = require("../data_base.js");
+
+const Router = express.Router();
+
+Router.route("/")
+  .get((req, res) => {
+    mysqlConnect.query("SELECT * from recipes", (err, row, field) => {
+      if (!err) {
+        res.status(200).send(row);
+      } else {
+        console.log(err);
+      }
+    });
+  })
+  .post((req, res) => {
+    let createAt = new Date().toISOString().slice(0, 10);
+    const { title, author, content } = req.body;
+    const post = { title, author, content, createAt };
+    mysqlConnect.query("INSERT INTO recipes SET?", post, (err, results) => {
+      if (!err) {
+        res.status(201).send(`post added with ID:${results.insertId}`);
+      } else {
+        console.log(err);
+      }
+    });
+  })
+  .put((req, res) => {
+    const id = req.params.id;
+    let createdAt = new Date().toISOString().slice(0, 10);
+    const { title, author, content } = req.body;
+    const post = { title, author, content, createAt };
+    mysqlConnect.query(
+      "UPDATE recipes SET ? WHERE id = ?",
+      [post, id],
+      (err, results) => {
+        if (!err) {
+          res.send(`post update with ID: ${id}`);
+        } else {
+          console.log(err);
+        }
+      }
+    );
+  })
+  .delete((req, res) => {
+    const id = req.params.id;
+    mysqlConnect.query(
+      "DELETE FROM recipes WHERE id = ?",
+      id,
+      (err, results) => {
+        if (!err) {
+          res.send(`Post deleted with ID:${id}`);
+        } else {
+          console.log(err);
+        }
+      }
+    );
+  });
+
+module.exports = Router;
--- server/server.js
+++ server/server.js
@@ -2,26 +2,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-const recipiesRouter = require("./routes/allRecipies.js");
-const getARecepiRouter = require("./routes/getRecipi.js");
-const postRecipiRouter = require("./routes/postRecipi.js");
-const deleteRecipiRouter = require("./routes/deleteRecipi.js");
-const updateRecipeRouter = require("./routes/updateRecipi.js");
 const app = express();
+
+const recipesRouter = require("./routes/recipes");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-//app.get('/',(req,res)=>{
-//	res.send("hello world");
-//});
-
-app.use("/api/recipies", recipiesRouter);
-app.use("/api/recipies", getARecepiRouter);
-app.use("/api/recipies", postRecipiRouter);
-app.use("/api/recipies", deleteRecipiRouter);
-app.use("/api/recipies", updateRecipeRouter);
+app.use("/api/recipes", recipesRouter);
 
 app.listen(3000, () => {
   console.log("server listening on port 3000");
