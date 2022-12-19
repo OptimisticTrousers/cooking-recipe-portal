6133c9d947cabd40c9c88ded8e68dd6f82c6708c
Abdoul <azizamine61@yahoo.fr>
2022-12-19 13:46:04 -0500

    updated the categories file

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -5,13 +5,30 @@ const Router = express.Router();
 const mysqlConnect = require("../data_base.js");
 
 Router.route("/").get((req, res) => {
+mysqlConnect.query("SELECT * from categories",(err,row,field)=>{
+	if(!err){
+		res.status(200).send(row);
+	}else{
+		console.log(err);
+	}
+})
+})
 
 }).post((req, res) => {
 
 }).put((req, res) => {
 
 }).delete((req, res) => {
-
-});
+   const id = req.params.id;
+	mysqlConnect.query("DELETE FROM categories WHERE id = ?",id
+		,(err,results)=>{
+			if(!err){
+				res.send(`category deleted with ID: ${id}`);
+			}else{
+				console.log(err);
+			}
+		})
+})
+})
 
 module.exports = Router
