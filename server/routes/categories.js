50fc327d20de57c75114e601c174529ccce841f2
OptimisticTrousers <tony.isern1@gmail.com>
2022-12-19 13:24:27 -0500

    Pushing boilerplate

---
+++ server/routes/categories.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const Router = express.Router();
+
+Router.route("/").get((req, res) => {
+
+}).post((req, res) => {
+
+}).put((req, res) => {
+
+}).delete((req, res) => {
+
+});
+
+module.exports = Router
--- server/server.js
+++ server/server.js
@@ -4,13 +4,15 @@ const cors = require("cors");
 
 const app = express();
 
-const recipesRouter = require("./routes/recipes");
+const recipesRouter = require("./routes/recipe");
+const categoriesRouter = require("./routes/categories");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use("/api/recipes", recipesRouter);
+app.use("/api/categories", categoriesRouter);
 
 app.listen(3000, () => {
   console.log("server listening on port 3000");
