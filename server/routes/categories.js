bf4c60bc055207ec233fdf054b8a31bb0361d1ac
OptimisticTrousers <tony.isern1@gmail.com>
2022-12-19 13:26:59 -0500

    Importing mysqlConnect file

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const Router = express.Router();
 
+const mysqlConnect = require("../data_base.js");
+
 Router.route("/").get((req, res) => {
 
 }).post((req, res) => {
