f7771ae72fadb051e0e9457b6498234b6be0c7b9
OptimisticTrousers <tony.isern1@gmail.com>
2022-12-22 17:08:21 -0500

    Finishing touches

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,10 +1,8 @@
-import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 
 function App() {
-
   return (
     <>
       <Navbar />
